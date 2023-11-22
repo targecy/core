eef5679ebc2c11caf16d83478b784b239d68f2a9
Martin Grabina <mgrabina@itba.edu.ar>
2023-11-22 16:31:00 -0300

    fix

--- packages/webapp/src/pages/api/auth/[...nextauth].ts
+++ packages/webapp/src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,8 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import { getCsrfToken } from 'next-auth/react';
 import { SiweMessage } from 'siwe';
 
+import { env } from '~~/env.mjs';
+
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default async function auth(req: any, res: any) {
@@ -24,7 +26,7 @@ export default async function auth(req: any, res: any) {
       async authorize(credentials) {
         try {
           const siwe = new SiweMessage(JSON.parse(credentials?.message || '{}'));
-          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL);
+          const nextAuthUrl = new URL(env.NEXTAUTH_URL);
 
           const result = await siwe.verify({
             signature: credentials?.signature || '',
@@ -57,7 +59,7 @@ export default async function auth(req: any, res: any) {
     session: {
       strategy: 'jwt',
     },
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: env.NEXTAUTH_SECRET,
     callbacks: {
       session({ session, token }: { session: any; token: any }) {
         session.address = token.sub;
