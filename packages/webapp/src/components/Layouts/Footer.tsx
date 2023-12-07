e91ae04394875412450ed1a21e4c0ca576dd2c8e
Martin Grabina <mgrabina@itba.edu.ar>
2023-12-07 16:19:25 -0300

    Add link to scanner

--- packages/webapp/src/components/Layouts/Footer.tsx
+++ packages/webapp/src/components/Layouts/Footer.tsx
@@ -1,7 +1,24 @@
+import Link from 'next/link';
+
+import { env } from '~~/env.mjs';
+
+const scannerUrl: Record<typeof env.NEXT_PUBLIC_VERCEL_ENV, string> = {
+  development: `http://localhost:8090`,
+  preview: `https://mumbai.polygonscan.com`,
+  production: `https://polygonscan.com`,
+};
+
 const Footer = () => {
   return (
     <div className="mt-auto p-6 pt-0 text-center dark:text-white-dark ltr:sm:text-right rtl:sm:text-right">
-      Targecy © {new Date().getFullYear()}. All rights reserved.
+      See{' '}
+      <Link
+        className=" cursor-pointer transition-all hover:text-primary"
+        target="_blank"
+        href={`${scannerUrl[env.NEXT_PUBLIC_VERCEL_ENV]}/address/${env.NEXT_PUBLIC_TARGECY_CONTRACT_ADDRESS}`}>
+        Targecy in scanner
+      </Link>
+      . All rights reserved © {new Date().getFullYear()}.
     </div>
   );
 };
