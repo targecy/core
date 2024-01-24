847e6002fbfcb29c0313cf79f6be24bf70337bd5
Martin Grabina <mgrabina@itba.edu.ar>
2024-01-24 17:41:41 -0300

    remove wall

--- packages/webapp/src/middleware.ts
+++ packages/webapp/src/middleware.ts
@@ -1,21 +1,18 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt';
 
-import { env } from './env.mjs';
+export function middleware(req: NextRequest) {
+  // const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  // if (env.NEXT_PUBLIC_VERCEL_ENV === 'development') return NextResponse.next();
 
-  if (env.NEXT_PUBLIC_VERCEL_ENV === 'development') return NextResponse.next();
-
-  if (!token?.isBetaUser) {
-    const requestedPage = req.nextUrl.pathname || '/';
-    const url = req.nextUrl.clone();
-    url.pathname = `/beta`;
-    url.search = `p=${requestedPage}`;
-    return NextResponse.redirect(url);
-  }
+  // if (!token?.isBetaUser) {
+  //   const requestedPage = req.nextUrl.pathname || '/';
+  //   const url = req.nextUrl.clone();
+  //   url.pathname = `/beta`;
+  //   url.search = `p=${requestedPage}`;
+  //   return NextResponse.redirect(url);
+  // }
 
   return NextResponse.next();
 }
