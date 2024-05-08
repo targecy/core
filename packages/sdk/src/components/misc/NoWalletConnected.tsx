947d740cc7197344af5f3397e230c119af048c16
faillacec <97037379+faillacec@users.noreply.github.com>
2024-05-08 11:09:23 -0500

    TAR-188 build sdk module

--- packages/sdk/src/components/misc/NoWalletConnected.tsx
+++ packages/sdk/src/components/misc/NoWalletConnected.tsx
@@ -1,13 +1,14 @@
-import { useConnectModal } from '@rainbow-me/rainbowkit';
+//import { useConnectModal } from '@rainbow-me/rainbowkit';
 
 export const NoWalletConnected = ({ caption }: { caption: string }) => {
-  const { openConnectModal } = useConnectModal();
+  // SDK IS NO USING THIS COMPONENT
+  //const { openConnectModal } = useConnectModal();
 
   return (
     <div>
-      <div onClick={openConnectModal} className="btn btn-primary w-fit">
+      {/* <div onClick={openConnectModal} className="btn btn-primary w-fit">
         {caption}
-      </div>
+      </div> */}
     </div>
   );
 };
