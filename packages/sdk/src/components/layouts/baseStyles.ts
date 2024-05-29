e01d7b4172685414deacf6abc485acb5a8ab7f78
faillacec <97037379+faillacec@users.noreply.github.com>
2024-05-29 10:44:25 -0500

    tar-193-improve-ads-styling-and-flexibility

---
+++ packages/sdk/src/components/layouts/baseStyles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const DivBase = styled.div`
+    position: 'absolute';
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: '10px';
+    background-color: 'rgba(0, 0, 0, 0.75)'; 
+    color: 'white';
+    border-radius: 16px;
+    opacity: 0;
+    transition: 'opacity 0.5s';
+    overflow: 'scroll';
+    height: '100%';
+    max-height: '100%';
+    `
+
